6ed68f3930de8f19e3a97fcf8588e5e16a2354c1
Hyeon-Yeong Chae <62824602+CheHyeonYeong@users.noreply.github.com>
2024-11-27 09:33:19 +0900

    Update 두 개 뽑아서 더하기.py

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,9 +1,9 @@
 def solution(numbers):
     answer = []
     numbers.sort();
+    # 모든 경우의 수 구함
     for i in range(len(numbers)) : 
         for j in range(i+1, len(numbers)):
-            num = numbers[i]+numbers[j]
-            if(num not in answer):
-                answer.append(num)
-    return sorted(answer)
+            answer.append(numbers[i]+numbers[j])
+    # set으로 중복 없애도 정렬해서 반환
+    return sorted(set(answer))
