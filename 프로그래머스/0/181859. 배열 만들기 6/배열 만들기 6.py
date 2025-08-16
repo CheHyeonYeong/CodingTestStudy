fbc14204419e2f239f7d5aab12662b0619e00395
Hyeon-Yeong Chae <62824602+CheHyeonYeong@users.noreply.github.com>
2025-08-16 16:51:30 +0900

    [level 0] Title: 배열 만들기 6, Time: 81.36 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -1,11 +1,10 @@
 def solution(arr):
     if not arr : 
         return [-1]
-    
-    stk, i = [], 0
+    stk = []
     
     for i in arr:
         stk.pop() if stk and stk[-1] == i else stk.append(i) 
         
         
-    return [-1] if len(stk) == 0 else stk
+    return stk or [-1]
