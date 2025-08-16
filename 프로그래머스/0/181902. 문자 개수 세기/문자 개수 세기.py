fcb1431d16bf8d51f4904cb8a130e12269eff547
CheHyeonYeong <gusdudco6@naver.com>
2025-08-16 16:18:05 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.24 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,9 +1,7 @@
 def solution(my_string):
-    answer = [0]*52
+    answer = [0] * 52
     for i in my_string:
-        if i.islower():
-            answer[ord(i)-71] = answer[ord(i)-71]+1
-        else : 
-            answer[ord(i)-65] = answer[ord(i)-65]+1
-        
+        o = ord(i)
+        answer[o - 65 if o < 97 else o - 71] += 1
+
     return answer
