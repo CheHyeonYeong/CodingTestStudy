c5d9aa8076f21b6ead3786847ab60b77958d03a6
Hyeon-Yeong Chae <62824602+CheHyeonYeong@users.noreply.github.com>
2025-09-09 20:44:35 +0900

    [level 1] Title: 체육복, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,23 @@
+def solution(n, lost, reserve):
+    
+    lost_copy = [x for x in lost]
+    
+    for i in lost:
+        if i in reserve:
+            lost_copy.remove(i)
+            reserve.remove(i)
+
+    lost.sort()
+    for s in lost:  
+        print(lost_copy, reserve)
+        if s in reserve and s in lost_copy:
+            lost_copy.remove(s)
+            reserve.remove(s )
+        elif (s - 1) in reserve and s in lost_copy:
+            lost_copy.remove(s)
+            reserve.remove(s - 1)
+        elif s + 1 in reserve and s in lost_copy:  
+            lost_copy.remove(s)
+            reserve.remove(s + 1)
+    print(lost_copy)
+    return n - len(lost_copy)
