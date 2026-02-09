2d954639071e8e32898351c05f72fac45994dae0
Hyeon-Yeong Chae <62824602+CheHyeonYeong@users.noreply.github.com>
2026-02-09 16:50:02 +0900

    [Bronze II] Title: 숫자의 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,25 @@
+
+dic = {
+    0:0,
+    1:0,
+    2:0,
+    3:0,
+    4:0,
+    5:0,
+    6:0,
+    7:0,
+    8:0,
+    9:0,
+}
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+number = str(a * b * c)
+
+for num in number:
+    dic[int(num)] += 1
+
+for i in range(10):
+    print(dic[i])
