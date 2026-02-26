944d06c678b05fec2ed785e6c84a6213ea3ba1b8
Hyeon-Yeong Chae <62824602+CheHyeonYeong@users.noreply.github.com>
2026-02-26 18:59:53 +0900

    [level 2] Title: 타겟 넘버, Time: 1006.83 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,34 +1,15 @@
-answer = 0
-
-def isTarget(numbers, ch):
-    v_target = 0
-    for i in range(len(numbers)):
-        if ch[i] == 0:
-            v_target += numbers[i]
-        else:
-            v_target -= numbers[i]
-    return v_target
-
-def DFS(L, numbers, ch, target):
-    global answer
-    
-    # 종료 조건: 모든 숫자를 다 봤을 때
-    if L == len(numbers):
-        if isTarget(numbers, ch) == target:
-            answer += 1
-        return
-    
-    # 현재 숫자를 더하는 경우 (ch[L] = 0)
-    ch[L] = 0
-    DFS(L+1, numbers, ch, target)
-    
-    # 현재 숫자를 빼는 경우 (ch[L] = 1)
-    ch[L] = 1
-    DFS(L+1, numbers, ch, target)
-
+from collections import deque
 def solution(numbers, target):
-    global answer
     answer = 0
-    ch = [0] * len(numbers)
-    DFS(0, numbers, ch, target)
+    dq = deque([(0,0)])
+    while dq: 
+        csum,idx = dq.popleft()
+        if idx == len(numbers):
+            if csum == target:
+                answer += 1
+        else:
+            dq.append([csum + numbers[idx] , idx+1])
+            dq.append([csum - numbers[idx] , idx+1])
     return answer
+
+
