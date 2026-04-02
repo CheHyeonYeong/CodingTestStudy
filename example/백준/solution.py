b6619b6c08f35e1b52facb8abfa287c92fe6c4be
CheHyeonYeong <gusdudco6@naver.com>
2026-04-02 15:05:02 +0900

    leetcode

--- example/백준/solution.py
+++ example/백준/solution.py
@@ -1,6 +1,6 @@
 ﻿import sys
 from io import StringIO
-
+from math import inf
 # ========== 로컬 테스트용 ==========
 # 제출 전에 아래 3줄 주석처리!
 test_input = open("input.txt").read()
@@ -11,21 +11,21 @@ input = sys.stdin.readline
 
 T = int(input())
 nums = list(map(int, input().split()))
-target = int(input())
 nums.sort()
+
 l = 0
 r = T -1
-answer = 0
-
+result = inf
+answer = [0,0]
 while l < r:
     csum = nums[l] + nums[r]
-    if csum == target:
-        answer += 1
-        l += 1
-        r -= 1
-    elif csum < target:
+    if abs(csum) < result:
+        result = abs(csum)
+        answer[0] = nums[l]
+        answer[1] = nums[r]
+
+    if csum < 0:
         l += 1
     else:
         r -= 1
-
-print(answer)
+print("{0} {1}".format(answer[0], answer[1]))
