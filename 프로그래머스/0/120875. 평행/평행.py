e41cfb84cb1a20cf8584f904150d12b13dafac43
Hyeon-Yeong Chae <62824602+CheHyeonYeong@users.noreply.github.com>
2026-06-20 15:49:52 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -0,0 +1,12 @@
+def parallel(a, b, c, d):
+    return (b[1]-a[1]) * (d[0]-c[0]) == \
+           (d[1]-c[1]) * (b[0]-a[0])
+
+def solution(dots):
+    if parallel(dots[0], dots[1], dots[2], dots[3]):
+        return 1
+    if parallel(dots[0], dots[2], dots[1], dots[3]):
+        return 1
+    if parallel(dots[0], dots[3], dots[1], dots[2]):
+        return 1
+    return 0
